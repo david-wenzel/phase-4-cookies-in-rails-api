c633b44ee1380537ef1c5cf2f730dc3ebd402a5b
David Wenzel <dwenzel95@gmail.com>
2023-03-05 16:33:50 -0600

    commit tree b6ee1e216d1bbd85dcf7cbcd529b3482d1626409

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module Phase4CookiesInRailsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
 
     # Adding back cookies and session middleware
     config.middleware.use ActionDispatch::Cookies
